a57cd0e7debd19e548067b9276cf6b1d20fa3d4a
DESKTOP-S0E0I8R\Kévin <kevin.arenas@outlook.fr>
2018-03-12 22:59:24 +0100

    opti code et prefab camera

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -47,18 +47,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             {
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v * m_CamForward + h * m_Cam.right;
+                m_Move = (v * m_CamForward + h * m_Cam.right)*0.5f;
             }
-            else
-            {
-                // we use world-relative directions in the case of no main camera
-                m_Move = v * Vector3.forward + h * Vector3.right;
-            }
-#if !MOBILE_INPUT
-            // walk speed multiplier
-            if (Input.GetKey(KeyCode.Z) || (Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.Q)) || (Input.GetKey(KeyCode.D))) m_Move *= 0.5f;
-#endif
-
+         
             // pass all parameters to the character control script
             m_Character.Move(m_Move, crouch, m_Jump);
             m_Jump = false;
