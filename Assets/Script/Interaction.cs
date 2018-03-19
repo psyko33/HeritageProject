978809a829a7bc49f5ac9ad819fe4745a3a4fff1
Mikael.PC <mikaeljouchoux@gmail.com>
2018-03-19 10:17:27 +0100

    ProBuilder OK c'est bon tg ta rienj kompris puteuhh

---
+++ Assets/Script/Interaction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour {
+
+    public GameObject USBKey;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void InteractObjectUSB()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
+
