b5c8a7745374bde7d77e5f18ca3c22332c9c9c52
DESKTOP-S0E0I8R\Kévin <kevin.arenas@outlook.fr>
2018-03-19 14:53:31 +0100

    Script changer camera pour crochetage

---
+++ Assets/Script/ActiverCrochetage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiverCrochetage : MonoBehaviour {
+
+    public GameObject playerCamera;
+    public GameObject player;
+    public GameObject cameraSerrure;
+
+    private bool IsUnlocked;
+    private bool CanOpen;
+
+	// Use this for initialization
+	void Start ()
+    {
+        IsUnlocked = false;
+        CanOpen = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Unlocked();
+        ExitLockPick();
+	}
+
+    void OnTriggerStay ( Collider other )
+    {
+       if (other.gameObject.tag == "Player" && Input.GetKeyDown("e"))
+        {
+            playerCamera.SetActive(false);
+            player.SetActive(false);
+            cameraSerrure.SetActive(true);
+            IsUnlocked = true;
+        } 
+    }
+
+    void Unlocked()
+    {
+        if (IsUnlocked == true && Input.GetKeyDown("a"))
+            Debug.Log("CHEST OPEN");
+            CanOpen = true;
+    }
+
+    void ExitLockPick()
+    {
+        if (CanOpen == true && Input.GetKeyDown("r"))
+        {
+            player.SetActive(true);
+            playerCamera.SetActive(true);
+            cameraSerrure.SetActive(false);
+            CanOpen = false;
+        }
+    }
+}
