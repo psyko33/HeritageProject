8ccc736e901d965d717292b525631b03862c2fb4
DESKTOP-S0E0I8R\Kévin <kevin.arenas@outlook.fr>
2018-04-06 10:38:23 +0200

    fazpifa

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -12,7 +12,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
-
+        public float speed;
 
         private void Start()
         {
@@ -41,7 +41,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             {
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = (v * m_CamForward + h * m_Cam.right)*0.5f;
+                m_Move = (v * m_CamForward + h * m_Cam.right)*speed;
             }
          
             // pass all parameters to the character control script
